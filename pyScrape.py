98ca350a88d0c4a67f29a4f807622ed42f6a4717
JSuelfl <Justin.Suelflow@bmo.com>
2016-01-25 12:19:06 -0600

    Update pyScrape.py

--- pyScrape.py
+++ pyScrape.py
@@ -1,7 +1,7 @@
 from lxml import html
 from lxml.etree import tostring
 from datetime import datetime, timedelta
-import requests, csv, re, time
+import requests, csv, re, time, traceback, sys
 ##  Function to scrape info
 def scrapeInfo(mainURL, mainContent, xPath):
     li = []
@@ -12,15 +12,21 @@ def scrapeInfo(mainURL, mainContent, xPath):
         link = mainLinksElements.get('href')
 ##  Takes '/' off end of mainURL
         url = mainURL[:-1]
-        if re.search(url, link) is None:
-            link = url + link
-##  Moves past pages that have are not postings
-        if re.search("https://my.backpage.com/classifieds", link) is not None or re.search("http://toronto.backpage.com/adult/?page=", link) is not None or re.search("http://my.backpage.com/classifieds", link) is not None or re.search(mainURL, link) is None:
+##  Moves past links that are not postings and add mainURL to links that need it
+        if re.search("backpage.com/classifieds", link) is not None or re.search("backpage.com/adult/\?page=", link) is not None or re.search("backpage.com/adult", link) is not None or re.search("backpage.com/online", link) is not None or link.endswith('/') or link == 'http://www.backpage.com':
             continue
+        elif re.search(mainURL, link) is None:
+            link = url + link
+            if link.count('/') < 3 or link.count('http') != 1:
+                continue
         phoneNumber = []
         emailAddress = []
         websiteLink = []
-        linkRequest = requests.get(link)
+        try:
+            linkRequest = requests.get(link)
+        except:
+            writeToLog(link + " did not send back a response.\n")
+            continue
         linkContent = html.fromstring(linkRequest.content)
 ##  Only grabs HTML tags with class = "postingBody"
         linkXPath = linkContent.xpath('//*[@class="postingBody"]')
@@ -101,28 +107,35 @@ def scrapeInfo(mainURL, mainContent, xPath):
                 s = s.get('href')
                 if s is not None:
                     if 'mailto:' in s:
-                        s = s[8:]
+                        s = s[7:]
                         emailAddress.append(s)
                     elif 'http' in s:
                         websiteLink.append(s)
+##  Makes link into tinyURL
+##        link = make_tinyURL(link)
 ##  Adds list item
-        link = make_tinyURL(link)
         li.append([phoneNumber,emailAddress,websiteLink,[link]])
     return li
 
-##  Function to create the CSV file
-def createCSV(liCSV, f1):
-    writer = csv.writer(f1, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=' ')
-##  Add a header row
-    writer.writerow(["PhoneNumber","Email_Address","Website","BackPage_Link"])
+##  Function to return if a list contains a number
+def is_number(st):
+    for s in st:
+        return s.isdigit()
+
+##  Function to write to the CSV file
+def writeToCSV(liCSV, writer):
     for i in liCSV:
         rowStr = ''
         num = []
+        site = []
         write = True
         for n in i:
             if isinstance(n, list):
                 if len(n) > 1:
-                    num = n
+                    if is_number(n):
+                        num = n
+                    else:
+                        site = n
                     write = False
                 else:
                     rowStr = rowStr + ''.join(n)
@@ -134,11 +147,19 @@ def createCSV(liCSV, f1):
             writer.writerow([rowStr])
         else:
             num = list(set(num))
+            site = list(set(site))
             rowCopy = rowStr
             for e in num:
                 li = [e]
                 rowStr = ''.join(li) + rowCopy
-                writer.writerow([rowStr])
+                rowCopy2 = rowStr
+                for s in site:
+                    lis = [s]
+                    r = rowCopy2.split(',')
+                    rowStr = r[0] + ',' + ''.join(lis) + ',' + r[2] + ',' + r[3]
+                    writer.writerow([rowStr])
+                if len(site) == 0:
+                    writer.writerow([rowStr])
 
 ##  Start timer
 def startTimer():
@@ -150,13 +171,17 @@ def endTimer(st):
     end = time.time()
     tot = end - st
     tot = ("{0:.1f}".format(round(tot,2)))
-    endStr = "It took " + str(tot) + " seconds to complete process."
+    endStr = "It took " + str(tot) + " seconds to complete scrape of: "
     return endStr
 
 ##  Make tiny URL to hide actual URL from url
 def make_tinyURL(url):
     request_url = ('http://tinyurl.com/api-create.php?url=' + url)
-    response = requests.get(request_url)
+    try:
+        response = requests.get(request_url)
+    except:
+        writeToLog('tinyURL did not respond with url for: ' + str(url) + '\n')
+        return url
     content = html.fromstring(response.content)
     return content.text
 
@@ -175,63 +200,73 @@ def writeToLog(text):
     logFile.close()
 
 ##  Main Function
-def main(mainURL):
+def main(mainURLList):
     startTime = time.time()
     currDate = datetime.now()
 ##  Make currDate Yesterday's date
     currDate = currDate - timedelta(days=1)
     currDate = currDate.strftime('%Y-%m-%d')
-    liData = []
-    increment = 1
 ##  Increment through 999 possible pages
     writeToLog("*************************** " + currDate + " ***************************\n")
     with open('/var/www/html/ScreenScrape.csv','w') as scrapeFile:
+        writer = csv.writer(scrapeFile, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=' ')
+##  Add a header row
+        writer.writerow(["PhoneNumber","Email_Address","Website","BackPage_Link"])
         try:
-            writeToLog("Main Scrape\n")
-            while increment < 1000:
-                writeToLog("Page: " + str(increment) + "\n")
-                if increment == 1:
-                    mainRequest = requests.get(mainURL + "adult/")
-                else:
-                    mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
-                mainContent = html.fromstring(mainRequest.content)
-                date = mainContent.xpath('//*[@class="date"]')
-                for dateStr in date:
-                    dateStr = tostring(dateStr)
-                    dateStr = re.search("\w{3}. \w{3}. \d{1,2}", dateStr)
-                    dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
-                    dateStr = dateStr.replace(year=datetime.now().year)
-                    dateStr = dateStr.strftime('%Y-%m-%d')
-##  Compare current date to date on webpage
-                if dateStr == currDate:
-                    startT = startTimer()
-                    liData.extend(scrapeInfo(mainURL, mainContent, '/html/body/div//*[@href]'))
-##  Extend liData to include anything from the sponsorBoxContent xPath
-                    liData.extend(scrapeInfo(mainURL, mainContent, '//*[@class="sponsorBoxContent"]/a'))
-                    writeToLog(endTimer(startT) + "\n")
-                elif currDate < dateStr and currDate <> '':
+            for mainURL in mainURLList:
+                liData = []
+                writeToLog("\nMain scrape of: " + mainURL + "\n")
+                startT = startTimer()
+                startPage = 0
+                endPage = 0
+                increment = 1
+                while increment < 1000:
+                    if increment == 1:
+                        mainRequest = requests.get(mainURL + "adult/")
+                    else:
+                        mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
+                    mainContent = html.fromstring(mainRequest.content)
+                    date = mainContent.xpath('//*[@class="date"]')
+                    dateStr = ''
+                    for dateStr in date:
+                        dateStr = tostring(dateStr)
+                        dateStr = re.search("\w{3}. \w{3}. \d{1,2}", dateStr)
+                        dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
+                        dateStr = dateStr.replace(year=datetime.now().year)
+                        dateStr = dateStr.strftime('%Y-%m-%d')
+                        if dateStr == currDate:
+                            break
+    ##  Compare current date to date on webpage
+                    if dateStr == currDate:
+                        if startPage == 0:
+                            startPage = increment
+                        liData.extend(scrapeInfo(mainURL, mainContent, '/html/body/div//*[@href]'))
+    ##  Extend liData to include anything from the sponsorBoxContent xPath
+                        liData.extend(scrapeInfo(mainURL, mainContent, '//*[@class="sponsorBoxContent"]/a'))
+                    elif currDate < dateStr and currDate <> '':
+                        increment = increment + 1
+                        continue
+                    else:
+                        endPage = increment
+                        writeToLog("Scraped pages: " + str(startPage) + " to " + str(endPage) + "\n")
+                        writeToLog("Remove dups from scrape of: " + mainURL + "\n")
+                        beforeDedup = len(liData)
+                        liData = removeDuplicates(liData)
+                        writeToLog(str(len(liData)) + " records of " + str(beforeDedup) + " left after deduplication\n")
+                        break
                     increment = increment + 1
-                    continue
-                else:
-                    startT = startTimer()
-                    writeToLog("Remove Dups from Scrape\n")
-                    writeToLog(len(liData) + " records from scrape\n")
-                    liData = removeDuplicates(liData)
-                    writeToLog(len(liData) + " records after deduplication\n")
-                    writeToLog(endTimer(startT) + "\n")
-                    startT = startTimer()
-                    writeToLog("Create CSV for Scrape\n")
-                    createCSV(liData, scrapeFile)
-                    writeToLog(endTimer(startT) + "\n")
-                    endTime = time.time()
-                    totTime = endTime - startTime
-                    totTime = ("{0:.1f}".format(round(totTime,2)))
-                    writeToLog("It took " + str(totTime) + " seconds to scrape yesterday's pages.\n")
-                    logFile.close()
-                increment = increment + 1
+                writeToLog(endTimer(startT) + mainURL + "\n")
+                writeToLog("Write to scrape to CSV\n")
+                writeToCSV(liData, writer)
+                time.sleep(30)
+                requests.get("http://www.google.com")
+            endTime = time.time()
+            totTime = endTime - startTime
+            totTime = ("{0:.1f}".format(round(totTime,2)))
+            writeToLog("It took " + str(totTime) + " seconds to scrape yesterday's postings.\n")
         except:
-            writeToLog("Unexpected error:" + sys.exc_info()[0] + "\n")
-            logFile.close()
+            e = traceback.format_exc()
+            writeToLog("Unexpected error:" + str(e) + "\n")
 
-mainURL = "http://toronto.backpage.com/"
-main(mainURL)
+mainURLList = ["http://alberta.backpage.com/", "http://britishcolumbia.backpage.com/", "http://manitoba.backpage.com/", "http://newbrunswick.backpage.com/", "http://stjohns.backpage.com/", "http://yellowknife.backpage.com/", "http://halifax.backpage.com/", "http://ontario.backpage.com/", "http://quebec.backpage.com/", "http://saskatchewan.backpage.com/", "http://whitehorse.backpage.com/", "http://illinois.backpage.com/"]
+main(mainURLList)
