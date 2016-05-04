e5e604b365f19e0f243c7223c75bbbd24ed1ed60
JSuelfl <Justin.Suelflow@bmo.com>
2016-05-04 14:44:15 -0500

    Production script archive webapp1 file - 5/4/2016
    Updated comments

--- Archive.sh
+++ Archive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-fCH="/Scrapes/`date +\%m\%d\%Y`_TorontoStar_Scrape.csv"
-fCBC="/Scrapes/`date +\%m\%d\%Y`_TorontoSun_Scrape.csv"
-fCTV="/Scrapes/`date +\%m\%d\%Y`_VancouverSun_Scrape.csv"
-
+# Create file variables for where they are located on the VM
+fCH="/Scrapes/`date +\%m\%d\%Y`_CalgaryHerald_Scrape.csv"
+fCBC="/Scrapes/`date +\%m\%d\%Y`_CBCNews_Scrape.csv"
+fCTV="/Scrapes/`date +\%m\%d\%Y`_CTVNews_Scrape.csv"
+# If files exist (-f) and are not empty (-s), then move the files to the Archive folder
 if [ -f $fCH ] && [ -s $fCH ]; then
 	sudo mv $fCH /Scrapes/Archive/
 fi
